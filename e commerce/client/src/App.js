25d19bd3af94c4d0dfbbdc573bb3815592ec3297
varsha-0228 <varsha.coursera@gmail.com>
2023-07-04 11:33:33 +0530

    project 4

--- e commerce/client/src/App.js
+++ e commerce/client/src/App.js
@@ -1,31 +1,40 @@
 import { useEffect } from "react";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  useLocation
-}from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./scenes/home/Home";
+import Navbar from "./scenes/global/Navbar";
+import Footer from "./scenes/global/Footer";
+import ItemDetails from "./scenes/ItemDetails/ItemDetails";
+import CartMenu from "./scenes/global/CartMenu";
+import Checkout from "./scenes/checkout/checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
 
-const ScrollToTop =() =>{
-  const {pathname } = useLocation();
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
 
-  useEffect(() =>{
-    window.scrollTo(0,0);
-  },[pathname])
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return null;
-}
+};
 
 function App() {
-  return <div className="app">
-    <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  </div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/success" element={<Confirmation />} />
+        </Routes>
+        <CartMenu />
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
